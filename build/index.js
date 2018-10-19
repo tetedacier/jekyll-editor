f9333314e26173ed3357eb268dbaee2ceb725568
tetedacier <fabien.jacq@gmail.com>
2018-10-19 14:23:57 +0200

    Set up base oauth subsystem

---
+++ build/index.js
@@ -0,0 +1,41 @@
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+const {
+  clientId
+} = require('./parameters')
+if (localStorage) {
+  if (localStorage.accessToken) {
+    document.getElementById('authorize').parentNode.removeChild(document.getElementById('authorize'))
+
+    appFrame = document.createElement('iframe')
+    appFrame.src = "/editor.html"
+    document.getElementsByTagName('body')[0].appendChild(appFrame)
+  } else {
+    if (localStorage.appPrivateKey) {
+      if (window.location.host !== 'tetedacier.github.io') {
+      //   alert('/!\\ `' + window.location.host + '` is a wrong realm')
+      // } else {
+
+        let authorizeButton = document.getElementById('authorize')
+        let eventName = 'click'
+        authorizeButton.addEventListener(eventName, (event) => {
+          window.href = 'https://github.com/login/oauth/authorize' + '?' +
+            'client_id=' + clientId +
+            'state=' + localStorage.getItem('defaultState')
+        })
+        localStorage.setItem('defaultState', 'st-'+ Math.random()*1e9)
+        authorizeButton.removeAttribute('disabled')
+      }
+    }
+    else {
+      console.info('ask gently for my app private key by email: tetedacier<fabien.jacq@gmail.com>')
+    }
+  }
+}
+
+},{"./parameters":2}],2:[function(require,module,exports){
+const clientId = '101ee5181784e405d2dc'
+module.exports = {
+  clientId
+}
+
+},{}]},{},[1]);
---
+++ build/oauth.js
@@ -0,0 +1,85 @@
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+const {
+  extractGithubOauthParameters,
+} = require('./utils')
+
+const {
+  githubAccessToken,
+} = require('./request')
+if (localStorage && localStorage.appPrivateKey) {
+  let parameter = extractGithubOauthParameters(window.location.search)
+  if(parameter !== null) {
+    if (parameter.state !== localStorage.defaultState) {
+      alert('/!\\ wrong authentication')
+    }
+    else {
+      githubAccessToken(parameter)
+    }
+  }
+  else {
+    alert('/!\\ missing authentication state and/or code parameter')
+  }
+}
+
+},{"./request":3,"./utils":4}],2:[function(require,module,exports){
+const clientId = '101ee5181784e405d2dc'
+module.exports = {
+  clientId
+}
+
+},{}],3:[function(require,module,exports){
+const {
+  clientId
+} = require('./parameters')
+
+const githubAccessToken = (parameter) => {
+    let oauth = new XMLHttpRequest();
+    oauth.addEventListener('load', (event) => {
+      if (oauth.readyState === XMLHttpRequest.DONE) {
+        if (oauth.status === 200) {
+          if (oauth.response === 'json') {
+            console.log(JSON.parse(oauth.reponseText))
+          } else {
+            console.log(oauth.reponseText)
+          }
+        } else {
+          alert(
+            '/!\\ missing authentication state and/or code parameter:\n' + oauth.reponseText
+          )
+        }
+      }
+    })
+
+    oauth.setRequestHeader('Accept', 'application/json')
+    oauth.setRequestHeader('Content-type', 'application/json')
+    oauth.open('POST', 'https://github.com/login/oauth/access_token');
+    oauth.send(JSON.stringify({
+      client_id: clientId,
+      client_secret: localStorage.appPrivateKey,
+      code: parameter.code
+    }))
+}
+module.exports = {
+  githubAccessToken
+}
+
+},{"./parameters":2}],4:[function(require,module,exports){
+const extractGithubOauthParameters = (locationSearch) => {
+    let parameter = {}
+    let queryString = locationSearch.match(
+      /^\?(state|code)=([^=\&]+)\&(state|code)=([^=\&]+)$/
+    )
+
+    if(queryString !== null) {
+      parameter[queryString[1]] = queryString[2]
+      parameter[queryString[3]] = queryString[4]
+      return parameter;
+    }
+
+    return null;
+}
+module.exports = {
+  extractGithubOauthParameters
+}
+
+},{}]},{},[1]);
