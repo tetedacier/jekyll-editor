9eb857cbb8129b5284709c2cdab88e9593deefa5
tetedacier <fabien.jacq@gmail.com>
2018-10-23 16:34:08 +0200

    Add github authorization request

--- src/index.js
+++ src/index.js
@@ -2,28 +2,45 @@ const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || process.env.server_port
+const request = require('request')
 
-// const request = require('request')
 const app = express()
+
 app.use(bodyParser.json())
 app.use(cors())
+const errorReponse = (res, message) => {
+  res.status(403)
+  res.set({'Content-Type':'application/json'})
+  return res.send(`{"message": "${message}"}`)
+}
 app.post('/authorize', (req, res) => {
   console.warn(req.body)
   if (req.body.code) {
-    let fake_gihub_access_token = '123445678'
-    res.status(200)
-    res.set({'Content-Type':'application/json'})
-    return res.send(
-      `{"gihub_access_token": "${
-        fake_gihub_access_token
-      }", "given_code": "${
-        req.body.code
-      }"}`
+    request.post(
+      {
+        url: 'https://github.com/login/oauth/access_token',
+        json:true,
+        body: {
+          client_id: process.env.client_id,
+          client_secret: process.env.client_secret,
+          code: req.body.code
+        }
+      }, (error, response, body) => {
+        if (error) {
+          return errorReponse(res, "invalid authorization parameters")
+        }
+
+        res.status(200)
+        res.set({'Content-Type':'application/json'})
+        return res.send(
+          `{"access_token": "${
+            reponse.access_token
+          }"}`
+        )
+      }
     )
   }
-  res.status(403)
-  res.set({'Content-Type':'application/json'})
-  return res.send(`{"message": "missing code parameter"}`)
+  return errorReponse(res, "missing code parameter")
 })
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`)
