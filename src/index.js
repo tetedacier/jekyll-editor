ad7864334f4d91a7337010600354f8ce154260bb
tetedacier <fabien.jacq@gmail.com>
2018-10-23 15:23:19 +0200

    Setup base skeleton for access token fetch

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser');
+// const request = require('request')
+const app = express()
+app.use(bodyParser.json())
+app.use(cors())
+app.post('/authorize', (req, res) => {
+  console.warn(req.body)
+  if (req.body.code) {
+    let fake_gihub_access_token = '123445678'
+    res.status(200)
+    res.set({'Content-Type':'application/json'})
+    return res.send(
+      `{"gihub_access_token": "${
+        fake_gihub_access_token
+      }", "given_code": "${
+        req.body.code
+      }"}`
+    )
+  }
+  res.status(403)
+  res.set({'Content-Type':'application/json'})
+  return res.send(`{"message": "missing code parameter"}`)
+})
+app.listen(process.env.server_port, () => {
+  console.log(`listening on port ${process.env.server_port}`)
+})
