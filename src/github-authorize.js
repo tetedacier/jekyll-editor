945b978d3671819e6f05dafca3d6c652bb3cd578
tetedacier <fabien.jacq@gmail.com>
2018-10-23 18:48:02 +0200

    Extract errors and github calls

---
+++ src/github-authorize.js
@@ -0,0 +1,38 @@
+const request = require('request')
+const errorReponse = (res, message) => {
+    res.status(403)
+    res.set({'Content-Type':'application/json'})
+    return res.send(`{"message": "${message}"}`)
+}
+const githubAuthorize = (res, code) => {
+  const {
+    client_id,
+    client_secret
+  } = process.env
+
+  return request.post(
+    {
+      url: 'https://github.com/login/oauth/access_token',
+      json:true,
+      body: {
+        client_id,
+        client_secret,
+        code
+      }
+    }, (error, response, body) => {
+      if (error) {
+        return errorReponse(res, "invalid authorization parameters")
+      }
+      res.status(200)
+      res.set({'Content-Type':'application/json'})
+      return res.json(
+        body
+      )
+    }
+  )
+}
+
+module.exports = {
+  githubAuthorize,
+  errorReponse
+}
--- src/index.js
+++ src/index.js
@@ -1,47 +1,23 @@
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser');
-const PORT = process.env.PORT || process.env.server_port
-const request = require('request')
-
+const PORT = process.env.PORT || 6000
+const {
+  githubAuthorize,
+  errorReponse
+} = require('./github-authorize')
 const app = express()
 
 app.use(bodyParser.json())
 app.use(cors())
-const errorReponse = (res, message) => {
-  res.status(403)
-  res.set({'Content-Type':'application/json'})
-  return res.send(`{"message": "${message}"}`)
-}
-app.post('/authorize', (req, res) => {
-  if (req.body.code) {
-    return request.post(
-      {
-        url: 'https://github.com/login/oauth/access_token',
-        json:true,
-        body: {
-          client_id: process.env.client_id,
-          client_secret: process.env.client_secret,
-          code: req.body.code
-        }
-      }, (error, response, body) => {
-        if (error) {
-          return errorReponse(res, "invalid authorization parameters")
-        }
 
-        res.status(200)
-        res.set({'Content-Type':'application/json'})
-        return res.send(
-          body
-          // `{"access_token": "${
-          //   body
-          // }"}`
-        )
-      }
-    )
+app.post('/authorize', (req, res) => {
+  if (req.body && req.body.code) {
+    return githubAuthorize(res, req.body.code)
   }
   return errorReponse(res, "missing code parameter")
 })
+
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`)
 })
